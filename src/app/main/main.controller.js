8d1735e29d8fe89416c2ffa4b4ac253bfb4ebdf9
sdip99@yahoo.com <sdip99@yahoo.com>
2019-03-28 01:52:47 -0400

    Changes on 28 mar

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,7 +2,17 @@ export class MainController {
   constructor ($http) {
     'ngInject';
     this.$http = $http;
+    this.getMessages();
   }
+
+  getMessages(){
+    var vm = this;
+    this.$http.get('http://localhost:5000/api/message').then(function(result){
+    vm.messages = result.data;  
+    console.log(result);
+    });
+  }
+
   postMessage(){
     this.$http.post('http://localhost:5000/api/message', {msg:this.message});
   }
